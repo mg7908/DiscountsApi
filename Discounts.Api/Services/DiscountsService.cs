ad1986a7761bd187c25aa742545e9ec39c96aa69
mgrx <mgrx@YOURMUM>
2026-06-03 22:53:59 +1000

    Refactor DiscountsService by moving the big calculations to their own methods

--- Discounts.Api/Services/DiscountsService.cs
+++ Discounts.Api/Services/DiscountsService.cs
@@ -1,3 +1,4 @@
+using Discounts.Api.Db.Entities;
 using Discounts.Api.Repositories;
 using Discounts.Api.Services.Models;
 
@@ -16,28 +17,16 @@ public class DiscountsService(IRepository repository) : IDiscountsService
         var pointsPromotion = await repository.GetPointsPromotionAsOf(transaction.TransactionDate);
         var discountPromotion = await repository.GetDiscountPromotionAsOf(transaction.TransactionDate);
 
-        // Loop through each product, and calculate the discount and total qualifying spend for earning points
-        foreach (var product in transaction.Basket)
-        {            
-            decimal discountApplied = 0;
-            
-            if (discountPromotion is not null)
-            {
-                discountApplied = discountPromotion.DiscountPromotionProducts.Count == 0 || discountPromotion.DiscountPromotionProducts.Any(x => x.ProductId == product.ProductId)
-                    ? product.Quantity * product.UnitPrice * (discountPromotion.DiscountPercent / 100)
-                    : 0m;
-            }
-
-            if (pointsPromotion is not null)
-            {
-                totalQualifyingPointsSpend += pointsPromotion.CategoryId is null || dbProducts[product.ProductId].CategoryId == pointsPromotion.CategoryId
-                    ? product.Quantity * product.UnitPrice - discountApplied
-                    : 0m;
-            }
+        // Loop through each item in the basket, and calculate the discount and total qualifying spend for earning points
+        foreach (var basketItem in transaction.Basket)
+        {
+            decimal discountApplied = CalculateDiscountApplied(discountPromotion, basketItem);
+            decimal qualifyingPointsSpend = CalculateQualifyingPointsSpend(pointsPromotion, dbProducts, basketItem, discountApplied);
 
             totalDiscountApplied += discountApplied;
-            totalAmount += product.Quantity * product.UnitPrice;
-            grandTotal += product.Quantity * product.UnitPrice - discountApplied;
+            totalAmount += basketItem.Quantity * basketItem.UnitPrice;
+            grandTotal += basketItem.Quantity * basketItem.UnitPrice - discountApplied;
+            totalQualifyingPointsSpend += qualifyingPointsSpend;
         }
 
         return new DiscountInfo(transaction.CustomerId,
@@ -48,5 +37,23 @@ public class DiscountsService(IRepository repository) : IDiscountsService
             grandTotal,
             (int)Math.Floor(totalQualifyingPointsSpend) * (pointsPromotion?.PointsPerDollarSpent ?? 0)
         );
+    }
+
+    private static decimal CalculateDiscountApplied(DiscountPromotion discountPromotion, BasketItem basketItem)
+    {
+        if (discountPromotion is null) return 0;
+
+        return discountPromotion.DiscountPromotionProducts.Count == 0 || discountPromotion.DiscountPromotionProducts.Any(x => x.ProductId == basketItem.ProductId)
+            ? basketItem.Quantity * basketItem.UnitPrice * (discountPromotion.DiscountPercent / 100)
+            : 0m;
+    }
+
+    private static decimal CalculateQualifyingPointsSpend(PointsPromotion pointsPromotion, Dictionary<string, Product> dbProducts, BasketItem basketItem, decimal discountApplied)
+    {
+        if (pointsPromotion is null) return 0;
+
+        return pointsPromotion.CategoryId is null || dbProducts[basketItem.ProductId].CategoryId == pointsPromotion.CategoryId
+            ? basketItem.Quantity * basketItem.UnitPrice - discountApplied
+            : 0m;
     }
 }
